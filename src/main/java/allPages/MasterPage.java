703425cfa902a4e1795e553720f89c4ca0156dab
arifovi22 <arifovi22@gmail.com>
2020-04-26 13:44:11 -0400

    second commit

--- src/main/java/allPages/MasterPage.java
+++ src/main/java/allPages/MasterPage.java
@@ -2,21 +2,25 @@ package allPages;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
 
 public class MasterPage {
-	WebDriver driver;
+	
+	 public WebDriver driver;
+	
 
 	@Parameters({"browser"})
 	@BeforeTest
@@ -30,6 +34,8 @@ public class MasterPage {
 					"C:\\Users\\Arif\\eclipse project\\GOTOMEETING2\\Browser\\geckodriver.exe" );
 			driver = new FirefoxDriver();
 		}
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
 		driver.get(url);
 	}
 
@@ -40,6 +46,17 @@ public class MasterPage {
 			driver.findElement(By.cssSelector(locator.split(":")[1])).click();
 		}
 	}
+	//04/26/2020
+	public String getText(String locator) {
+		String actual = "";
+		if (locator.contains("xpath")) {
+			actual = driver.findElement(By.xpath(locator.split(":")[1])).getText();
+		} else if (locator.contains("cssSelector")) {
+		actual =	driver.findElement(By.cssSelector(locator.split(":")[1])).getText();
+		}
+		return actual;
+	}
+	
 	public void typeOnElement(String locator, String value) {
 		if (locator.contains("xpath")) {
 			driver.findElement(By.xpath(locator.split(":")[1])).sendKeys(value,Keys.ENTER);
@@ -53,6 +70,28 @@ public class MasterPage {
 		File getTypeFile = screenshot.getScreenshotAs(OutputType.FILE);
 		File file = new File("C:\\Users\\Arif\\eclipse project\\GOTOMEETING2\\ScreenShort\\"+filename+".png");
 		FileUtils.copyFile(getTypeFile, file);
+	}
+	//04/26/2020
+	public void switchFrame(int index) {
+		driver.switchTo().frame(index);
+	}
+	
+	public void scrollDown(int wide, int length) {
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy("+wide+","+length+")");
+	}
+	public void mouseHover(WebElement target) {
+		Actions actions = new Actions(driver);
+		actions.moveToElement(target).build().perform();
+	}
+	public WebElement webElement(String locator) {
+		WebElement element = null;
+		if (locator.contains("xpath")) {
+		element = driver.findElement(By.xpath(locator.split(":")[1]));
+		} else if (locator.contains("cssSelector")) {
+		element = driver.findElement(By.cssSelector(locator.split(":")[1]));
+		}
+		return element;
 	}
 
 }
--- src/test/java/allTest/TakeScreenShort.java
+++ src/test/java/allTest/TakeScreenShort.java
@@ -1,18 +1,52 @@
 package allTest;
 
+
 import java.io.IOException;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import allPages.MasterPage;
 
+
 public class TakeScreenShort {
-	@Test
+
+	MasterPage masterPage = new MasterPage();
+	//@Test
 	public void screenShort() throws IOException {
-		MasterPage masterPage = new MasterPage();
 		masterPage.openBrowser("chrome","https://jqueryui.com/selectable/");
 		masterPage.takeScreenshors("take1");
+		masterPage.driver.close();
 		
 	}
+	//@Test //04/26/2020
+	public void validateColour() throws IOException, InterruptedException {
+		String button = "xpath://button[@id='button']";
+		String text  = "xpath://p[contains(text(),'Etiam libero neque')]";
+		masterPage.openBrowser("chrome","https://jqueryui.com/animate/");
+		masterPage.switchFrame(0);
+		masterPage.clickOnElement(button);
+		Thread.sleep(2000);
+		String actual = masterPage.getText(text);
+		Assert.assertEquals(actual,
+		"Etiam libero neque, luctus a, eleifend nec, semper at, lorem. Sed pede. Nulla lorem metus, adipiscing ut, luctus sed, hendrerit vitae, mi.");
+		masterPage.driver.close();
+	}
+	@Test
+	public void mouseHover() {
+		String item = "xpath://div[@id='ui-id-17']";
+		masterPage.openBrowser("chrome","https://jqueryui.com/themeroller/");
+		masterPage.scrollDown(0,2500);
+
+		WebElement element = masterPage.webElement(item);
+		masterPage.mouseHover(element);
+		
+		
+	}
+	
+	
+	
 
 }
